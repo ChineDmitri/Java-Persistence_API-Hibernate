697806fea0235f24b6faee504d9b40e0edcdc4c1
Dmitri CHINE <dmitri.chine@gmail.com>
2024-04-10 00:35:22 +0200

    Hello wordl to db

--- src/main/java/com/chinedmitri/App.java
+++ src/main/java/com/chinedmitri/App.java
@@ -1,10 +1,35 @@
 package com.chinedmitri;
 
+import com.chinedmitri.model.Message;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.transaction.TransactionManager;
+import javax.transaction.UserTransaction;
+
 /**
  * Hello world!
  */
 public class App {
+    
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("HelloWorldPU");
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Start app");
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Message msg1 = new Message();
+        msg1.setText("Hello JPA et Hibernate");
+
+        em.persist(msg1);
+        em.getTransaction().commit();
+        em.close();
+
+
+        System.out.println(msg1.getText());
     }
 }
